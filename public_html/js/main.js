0bee342452962082e8ca6117aea307b2319f3cf1
Sofija <sofijasokic91@gmail.com>
2020-03-26 22:04:49 +0100

    Initialization of header

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,7 +1,28 @@
 $(document).ready(function(){
     
     
+    function animation() {
+        var windowHeight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            let position = $(this).offset().top;
+            let animation = $(this).attr('data-animation');
+            let delay = $(this).attr('data-delay');
+            if (position < scroll + windowHeight - 100) {
+                $(this).css('animation-delay', delay);
+                $(this).addClass(animation);
+            }
+        });
+    }
+    animation();
+    $(window).scroll(function () {
+        animation();
+    });
     
     
-})
+    
+    
+    
+    
+});
 
